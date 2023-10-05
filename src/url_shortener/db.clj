53454868c7d8b9698458a1811b976f16264e48fc
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-05 17:48:08 +0800

    feat: Working db

--- src/url_shortener/db.clj
+++ src/url_shortener/db.clj
@@ -1,5 +1,7 @@
 (ns url-shortener.db
-  (:require [clojure.java.jdbc :as j]))
+  (:require [clojure.java.jdbc :as j]
+            [honey.sql :as sql]
+            [honey.sql.helpers :refer :all]))
 
 (def mysql-db {:host "localhost"
                :port 3306
@@ -8,5 +10,46 @@
                :user "root"
                :password ""})
 
-(j/query mysql-db
-  ["select * from redirects"])
+(def select-from-redirect
+  (-> (select :*)
+      (from :redirects)
+      (sql/format)))
+
+(defn query [q]
+  (j/query mysql-db q))
+
+(defn insert! [q]
+  (j/db-do-prepared mysql-db q))
+
+(defn insert-redirect! [slug url]
+  (insert! (-> (insert-into :redirects)
+               (columns :slug :url)
+               (values [[slug, url]])
+               (sql/format))))
+
+(defn query-redirect [slug]
+  (-> (query (-> (select :url)
+             (from :redirects)
+             (where [:= :slug slug])
+             (sql/format)))
+      first
+      :url))
+
+; Query to db
+(query select-from-redirect)
+
+; Workspace for testing code
+(comment
+  (query (-> (select :*)
+             (from :redirects)
+             (sql/format)))
+  (insert! (-> (insert-into :redirects)
+               (columns :slug :url)
+               (values
+                [["gh", "https://github.com"]
+                 ["gg", "https://google.com"]])
+               (sql/format {:pretty true})))
+  (insert-redirect! "yt" "https://youtube.com")
+  (query-redirect "yt")
+  )
+
