671df49290edea0570b4fe47f30638b6bb4bf0d9
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-05 19:20:11 +0800

    feat: Working hello world api

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,7 +1,23 @@
 (ns url-shortener.core
+  (:require [ring.adapter.jetty :as ring-jetty]
+            [reitit.ring :as ring]
+            [muuntaja.core :as m]
+            [reitit.ring.middleware.muuntaja :as muuntaja])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def app
+  (ring/ring-handler 
+   (ring/router 
+    [ "/" 
+     ["" {:handler (fn [req] {:body "hello" :status 200})}]] 
+    {:data {:muuntaja m/instance 
+            :middleware [muuntaja/format-middleware]}})))
+
+(defn start []
+  (ring-jetty/run-jetty #'app {:port 3001
+                             :join? false}))
+
+(defn -main [& args]
+  (def server (start)))
+
+(comment(.stop server))
