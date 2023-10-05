75d37c4ca202e4d37bc533334940e54ff3020e21
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-05 20:21:16 +0800

    feat: Working url shortener

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,15 +1,34 @@
 (ns url-shortener.core
   (:require [ring.adapter.jetty :as ring-jetty]
             [reitit.ring :as ring]
+            [ring.util.response :as r]
             [muuntaja.core :as m]
-            [reitit.ring.middleware.muuntaja :as muuntaja])
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [url-shortener.db :as db]
+            [url-shortener.slug :as slug])
   (:gen-class))
+
+(defn redirect [req]
+  (let [slug (get-in req [:path-params :slug])
+        url (db/query-redirect slug)]
+    (if url
+      (r/redirect url 307)
+      (r/not-found "Not found"))))
+
+(defn create-redirect [req]
+  (let [url (get-in req [:body-params :url])
+        slug (slug/generate-slug)]
+    (db/insert-redirect! slug url)
+    (r/response (str "http://localhost:3001/" slug))))
 
 (def app
   (ring/ring-handler 
    (ring/router 
-    [ "/" 
-     ["" {:handler (fn [req] {:body "hello" :status 200})}]] 
+    [ "/"
+     [":slug" redirect]
+     ["api/"
+      ["redirect" {:post create-redirect}]]
+     ["" {:handler (fn [req] {:body "welcome to url shortener" :status 200})}]] 
     {:data {:muuntaja m/instance 
             :middleware [muuntaja/format-middleware]}})))
 
